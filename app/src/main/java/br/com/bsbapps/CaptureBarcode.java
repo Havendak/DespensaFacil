1aa7c9182d12073c6a0f397876d3ba736ebf08a8
Havendak <abecklas@gmail.com>
2016-11-15 17:41:51 -0200

    Adiocinando activity de captura

---
+++ app/src/main/java/br/com/bsbapps/CaptureBarcode.java
@@ -0,0 +1,13 @@
+package br.com.bsbapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CaptureBarcode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture_barcode);
+    }
+}
