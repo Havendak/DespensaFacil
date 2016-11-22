91cc27ede07e4ab737753ac9946b9476ccc1b56d
Andre Becklas <abecklas@gmail.com>
2016-11-22 00:33:33 -0200

    Mudança para biblioteca Zxing

---
+++ app/src/main/java/br/com/bsbapps/despensafacil/barra_superior.java
@@ -0,0 +1,13 @@
+package br.com.bsbapps.despensafacil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class barra_superior extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barra_superior);
+    }
+}
