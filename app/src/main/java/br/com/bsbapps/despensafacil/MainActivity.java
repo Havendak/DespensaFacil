90fd8e3e88bbdebfca20af9ea1e94356e4e6d5b0
procaci <procaci@gmail.com>
2016-11-28 20:35:59 -0200

    database

--- app/src/main/java/br/com/bsbapps/despensafacil/MainActivity.java
+++ app/src/main/java/br/com/bsbapps/despensafacil/MainActivity.java
@@ -9,11 +9,14 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseHelper mydb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mydb = new DatabaseHelper(this);
 
     }
 
